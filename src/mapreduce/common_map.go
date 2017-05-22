69559f2bc5b0b3af2aad27cfd42ebcd9c78664bb
Han Yu <hany@garena.com>
2017-05-22 11:48:42 +0800

    pass sequential version

--- src/mapreduce/common_map.go
+++ src/mapreduce/common_map.go
@@ -1,6 +1,12 @@
 package mapreduce
 
-import "hash/fnv"
+import (
+	"encoding/json"
+	"hash/fnv"
+	"io/ioutil"
+	"log"
+	"os"
+)
 
 // doMap reads the assigned input file, calls mapF for that file's contents,
 // and writes the output into nReduce output bins.
@@ -26,6 +32,38 @@ func doMap(
 	//     err := enc.Encode(&kv)
 	//
 	// Remember to close the file after you have written all the values!
+	file, err := os.Open(inFile)
+	if err != nil {
+		log.Fatal("open in file error")
+	}
+	content, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatal("read file error")
+	}
+	kvs := mapF(inFile, string(content))
+	// prepare files
+	encMap := make([]*json.Encoder, nReduce)
+	for j := 0; j < nReduce; j++ {
+		name := reduceName(jobName, job, j)
+		debug("reduce name = %s\n", name)
+		f, err := os.Create(name)
+		if err != nil {
+			log.Fatalf("create file error %s", err)
+			return
+		}
+		defer f.Close()
+		enc := json.NewEncoder(f)
+		encMap[j] = enc
+	}
+
+	for _, kv := range kvs {
+		enc := encMap[int(ihash(kv.Key))%nReduce]
+		err := enc.Encode(kv)
+		if err != nil {
+			log.Printf("warning enc encode error %s\n", err)
+		}
+	}
+
 }
 
 func ihash(s string) uint32 {
--- src/mapreduce/common_reduce.go
+++ src/mapreduce/common_reduce.go
@@ -1,5 +1,13 @@
 package mapreduce
 
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"os"
+	"sort"
+)
+
 // doReduce reads the map outputs for the bin assigned to this job, sorts the
 // outputs by key, calls reduceF for each key, and writes the output to disk.
 func doReduce(
@@ -25,4 +33,46 @@ func doReduce(
 	// 	enc.Encode(KeyValue{key, reduceF(...)})
 	// }
 	// file.Close()
+	kvmap := make(map[string][]string)
+	for i := 0; i < nMap; i++ {
+		// map result file name for this reducer
+		name := reduceName(jobName, i, job)
+		f, err := os.Open(name)
+		if err != nil {
+			log.Fatalf("open map file error %s\n", err)
+		}
+		defer f.Close()
+		dec := json.NewDecoder(f)
+		for {
+			kv := &KeyValue{}
+			err := dec.Decode(kv)
+			if err == io.EOF {
+				break
+			}
+			if _, ok := kvmap[kv.Key]; !ok {
+				kvmap[kv.Key] = make([]string, 0)
+			}
+			kvmap[kv.Key] = append(kvmap[kv.Key], kv.Value)
+		}
+	}
+	var keys []string
+	for k, _ := range kvmap {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	mName := mergeName(jobName, job)
+	f, err := os.Create(mName)
+	if err != nil {
+		log.Fatalf("create file error %s", err)
+		return
+	}
+	defer f.Close()
+	enc := json.NewEncoder(f)
+	for _, key := range keys {
+		res := reduceF(key, kvmap[key])
+		err := enc.Encode(KeyValue{Key: key, Value: res})
+		if err != nil {
+			log.Printf("warning enc encode error %s\n", err)
+		}
+	}
 }
