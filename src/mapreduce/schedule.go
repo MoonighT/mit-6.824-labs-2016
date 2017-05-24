e1105306af047245f7193f71c5576aa79da43e95
Han Yu <hany@garena.com>
2017-05-24 14:15:10 +0800

    add raft version and sechduler

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -1,11 +1,15 @@
 package mapreduce
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 // schedule starts and waits for all jobs in the given phase (Map or Reduce).
 func (mr *Master) schedule(phase jobPhase) {
 	var njobs int
 	var nios int // number of inputs (for reduce) or outputs (for map)
+	var otherphase int
 	switch phase {
 	case mapPhase:
 		njobs = len(mr.files)
@@ -21,8 +25,33 @@ func (mr *Master) schedule(phase jobPhase) {
 	// them have been completed successfully should the function return.
 	// Remember that workers may fail, and that any given worker may finish
 	// multiple jobs.
-	//
-	// TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
-	//
+	var wg sync.WaitGroup
+	wg.Add(njobs)
+
+	type Job struct {
+		file  string
+		index int
+	}
+	for i, _ := range mr.files {
+		go func(index int) {
+			w := <-mr.registerChannel
+			arg := &DoJobArgs{
+				JobName:       mr.jobName,
+				File:          mr.files[index],
+				Phase:         phase,
+				JobNumber:     index,
+				NumOtherPhase: nios,
+			}
+			ok := call(w, "Worker.DoJob", arg, new(struct{}))
+			if !ok {
+				debug("call not ok")
+			} else {
+				mr.registerChannel <- w
+				wg.Done()
+				break
+			}
+		}(i)
+	}
+	wg.Wait()
 	fmt.Printf("Schedule: %v phase done\n", phase)
 }
